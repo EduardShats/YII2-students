c2a2308ff7f850f4f865845d76f64265b9cc0c50
EduardShats <shats_eduard@mail.ru>
2018-07-04 17:31:22 +0700

    1

--- views/student/student.php
+++ views/student/student.php
@@ -11,34 +11,3 @@ $this->title = $student['surname'].' '.$student['forename'].' '.$student['patron
 <h2>Курс: <?= $student['course']?></h2>
 <h2>Информация о студенте: </h2>
 <p><?= $student['info']?></p>
-<div class="demo">
-    <?php/* foreach($universitys as $key =>$value)
-    {
-        echo "
-        <input class=\"hide\" id=\"hd-$key\" type=\"checkbox\">
-        <label for=\"hd-$key\">", $value['name'] ,"</label>
-        <div>
-            <table>";
-        foreach($students as $value1)
-        {
-            if($value1['university_id'] == $value['id'])
-            {
-                echo "
-                <tr><a href='", Url::toRoute(['student/student', 'id' => $value1['id']]) ,"'>
-                    <td>",$value1['surname']," </td>
-                    <td>",$value1['forename']," </td>
-                    <td>",$value1['patronymic']," </td>
-                    <td>",$value1['profession']," </td>
-                    <td>",$value1['course']," </td>
-                </a></tr>";
-            }
-        }
-        echo "
-            </table>
-        </div>
-        <br />";
-    }
-    */?>
-
-
-</div>
