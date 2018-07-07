5b408d853935a1591feb4e7b7a79c7dd61a526ad
EduardShats <shats_eduard@mail.ru>
2018-07-07 22:11:11 +0700

    1

--- views/student/add.php
+++ views/student/add.php
@@ -2,21 +2,7 @@
 use yii\widgets\ActiveForm;
 use yii\helpers\Html;
 
-?>
 
-<?php if(Yii::$app->session->hasFlash('success')): ?>
-    <div class="alert alert-success alert-dismissable">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-        <?= Yii::$app->session->getFlash('success') ?>
-    </div>
-<?php endif;?>
-<?php if(Yii::$app->session->hasFlash('error')): ?>
-    <div class="alert alert-danger alert-dismissable">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-        <?= Yii::$app->session->getFlash('error') ?>
-    </div>
-<?php endif;?>
-<?php
 $form = ActiveForm::begin();
 echo $form->field($model, 'surname');
 echo $form->field($model, 'forename');
